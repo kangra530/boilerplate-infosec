b0679ab22067601df23cebf0d0ba67dd3e8d659c
Katie Fletcher <77756012+kfletch2024@users.noreply.github.com>
2024-01-02 17:09:30 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,19 @@
 const express = require('express');
+
+import express from "express";
+import helmet from "helmet";
+
 const app = express();
+
+// Use Helmet!
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(8000);
+
 
 
 
